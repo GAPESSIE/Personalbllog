9fe911731d7780f3a7b15690cc8ab86094743884
Gapessie Kevin <gapessiek@gmail.com>
2024-06-18 00:00:54 +0200

    Kevin accomplished the task customize the admin site

--- personalblog/Kevin/admin.py
+++ personalblog/Kevin/admin.py
@@ -3,6 +3,17 @@ from .models import *
 # Register your models here.
 
 admin.site.register(User)
-admin.site.register(KevinPost)
-admin.site.register(KevinComment)
+# admin.site.register(KevinPost)
+# admin.site.register(KevinComment)
 
+@admin.register(KevinPost)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'content', 'publication_date')
+    search_fields = ('user', 'title')
+    list_filter = ('title',  'user')
+
+@admin.register(KevinComment)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'content', 'timestamp')
+    search_fields = ('user', 'post')
+    list_filter = ('post',  'user')
